643c010f31716ecb4d8f5375fbc23e2b3b6b0cd8
agastsya <agastsya007@gmail.com>
2023-08-22 19:24:21 +0530

    fixed some basic issues

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -40,104 +40,112 @@ const Cart = () => {
   return (
     <>
       <Stack direction={'column'} minH={'100vh'}>
-        <Button {...buttonstyle} w={'fit-content'} mx={'auto'} mt={'5'}>
-          Checkout Now
-        </Button>
-        {cartItems.map(item => (
-          <VStack w={'full'}>
+        {cartItems.length === 0 ? (
+          <Text mx={'auto'} fontSize={'4xl'}>
+            Cart Is Empty
+          </Text>
+        ) : (
+          <>
+            <Button {...buttonstyle} w={'fit-content'} mx={'auto'} mt={'5'}>
+              Checkout Now
+            </Button>
+            {cartItems.map(item => (
+              <VStack w={'full'}>
+                <Box
+                  w={'80vw'}
+                  h={'fit-content'}
+                  my={'10'}
+                  style={{ 'box-shadow': '10px 10px' }}
+                  border="2px solid black"
+                  borderRadius={['0', '10px']}
+                  bgColor={'whiteAlpha.500'}
+                  display={'flex'}
+                  flexDirection={'column'}
+                  justifyContent={'center'}
+                  px={['3', '20']}
+                  bgGradient="linear(to-l, #20e3b2, #29ffc6)"
+                >
+                  <HStack>
+                    <VStack w={'40%'}>
+                      <Text fontWeight={'semibold'}>{item.title}</Text>
+
+                      <Image
+                        w={'32'}
+                        src={item.image}
+                        style={{ 'box-shadow': '5px 5px' }}
+                        border="2px solid black"
+                        mb={'5'}
+                      />
+                      <Text fontWeight={'bold'} fontSize={'2xl'}>
+                        ${item.price}
+                      </Text>
+                    </VStack>
+                    <VStack w={'30%'}>
+                      <Button
+                        {...buttonstyle}
+                        w={'10'}
+                        my={'5'}
+                        onClick={() => addToCart(item)}
+                      >
+                        +
+                      </Button>
+                      <Text>{item.quantity}</Text>
+                      <Button
+                        {...buttonstyle}
+                        w={'10'}
+                        onClick={() => reduceCart(item)}
+                      >
+                        -
+                      </Button>
+                    </VStack>
+                    <VStack w={'30%'}>
+                      <Button
+                        {...buttonstyle}
+                        borderRadius={'full'}
+                        onClick={() => removeCartItem(item.id)}
+                      >
+                        X
+                      </Button>
+                    </VStack>
+                  </HStack>
+                </Box>
+              </VStack>
+            ))}
+
+            <Button
+              {...buttonstyle}
+              w={'fit-content'}
+              mx={'auto'}
+              mb={'5'}
+              onClick={() => {
+                emptyCart();
+              }}
+            >
+              Remove All From Cart
+            </Button>
             <Box
-              w={'80vw'}
-              h={'fit-content'}
-              my={'10'}
+              w={'full'}
+              h={'20vh'}
+              display={'flex'}
+              flexDirection={'column'}
               style={{ 'box-shadow': '10px 10px' }}
+              mb={'10'}
               border="2px solid black"
               borderRadius={['0', '10px']}
               bgColor={'whiteAlpha.500'}
-              display={'flex'}
-              flexDirection={'column'}
-              justifyContent={'center'}
-              px={['3', '20']}
+              px={('0', '2rem')}
+              py={'30'}
               bgGradient="linear(to-l, #20e3b2, #29ffc6)"
             >
-              <HStack>
-                <VStack w={'40%'}>
-                  <Text fontWeight={'semibold'}>{item.title}</Text>
-
-                  <Image
-                    w={'32'}
-                    src={item.image}
-                    style={{ 'box-shadow': '5px 5px' }}
-                    border="2px solid black"
-                    mb={'5'}
-                  />
-                  <Text fontWeight={'bold'} fontSize={'2xl'}>
-                    ${item.price}
-                  </Text>
-                </VStack>
-                <VStack w={'30%'}>
-                  <Button
-                    {...buttonstyle}
-                    w={'10'}
-                    my={'5'}
-                    onClick={() => addToCart(item)}
-                  >
-                    +
-                  </Button>
-                  <Text>{item.quantity}</Text>
-                  <Button
-                    {...buttonstyle}
-                    w={'10'}
-                    onClick={() => reduceCart(item)}
-                  >
-                    -
-                  </Button>
-                </VStack>
-                <VStack w={'30%'}>
-                  <Button
-                    {...buttonstyle}
-                    borderRadius={'full'}
-                    onClick={() => removeCartItem(item.id)}
-                  >
-                    X
-                  </Button>
-                </VStack>
-              </HStack>
+              <Text mx={'auto'} fontSize={'3xl'} fontWeight={'semibold'}>
+                Shipping Price : ${originalPrice.toFixed(2)}
+              </Text>
+              <Text mx={'auto'} fontSize={'3xl'} fontWeight={'semibold'}>
+                Final Amount : ${total.toFixed(2)}
+              </Text>
             </Box>
-          </VStack>
-        ))}
-
-        <Button
-          {...buttonstyle}
-          w={'fit-content'}
-          mx={'auto'}
-          mb={'5'}
-          onClick={() => {
-            emptyCart();
-          }}
-        >
-          Remove All From Cart
-        </Button>
-        <Box
-          w={'full'}
-          h={'20vh'}
-          display={'flex'}
-          flexDirection={'column'}
-          style={{ 'box-shadow': '10px 10px' }}
-          mb={'10'}
-          border="2px solid black"
-          borderRadius={['0', '10px']}
-          bgColor={'whiteAlpha.500'}
-          px={('0', '2rem')}
-          py={'30'}
-          bgGradient="linear(to-l, #20e3b2, #29ffc6)"
-        >
-          <Text mx={'auto'} fontSize={'3xl'} fontWeight={'semibold'}>
-            Shipping Price : ${originalPrice.toFixed(2)}
-          </Text>
-          <Text mx={'auto'} fontSize={'3xl'} fontWeight={'semibold'}>
-            Final Amount : ${total.toFixed(2)}
-          </Text>
-        </Box>
+          </>
+        )}
       </Stack>
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { Link, NavLink } from 'react-router-dom';
 import { FiShoppingBag, FiMapPin, FiShoppingCart } from 'react-icons/fi';
+import { BsCart4 } from 'react-icons/bs';
 import { Text, Avatar, HStack, VStack, Box, Center } from '@chakra-ui/react';
 import styled from 'styled-components';
 import '../styles/Header.scss';
@@ -152,7 +153,15 @@ const Header = () => {
                   mt={'-1.5vh'}
                   className="nav_link"
                 >
-                  <FiShoppingCart size={'30'} />
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="32"
+                    height="32"
+                    fill="#065f46"
+                    viewBox="0 0 256 256"
+                  >
+                    <path d="M222.14,58.87A8,8,0,0,0,216,56H54.68L49.79,29.14A16,16,0,0,0,34.05,16H16a8,8,0,0,0,0,16h18L59.56,172.29a24,24,0,0,0,5.33,11.27,28,28,0,1,0,44.4,8.44h45.42A27.75,27.75,0,0,0,152,204a28,28,0,1,0,28-28H83.17a8,8,0,0,1-7.87-6.57L72.13,152h116a24,24,0,0,0,23.61-19.71l12.16-66.86A8,8,0,0,0,222.14,58.87ZM96,204a12,12,0,1,1-12-12A12,12,0,0,1,96,204Zm96,0a12,12,0,1,1-12-12A12,12,0,0,1,192,204Zm4-74.57A8,8,0,0,1,188.1,136H69.22L57.59,72H206.41Z"></path>
+                  </svg>
                 </Text>
                 <Text
                   fontSize={'smaller'}
