5174995a2bbaf8038b1f179d00ed2bb08c2ac743
agastsya <agastsya007@gmail.com>
2023-09-21 10:25:57 +0530

    Testing login loader

--- src/components/userAccount/Login.jsx
+++ src/components/userAccount/Login.jsx
@@ -12,6 +12,7 @@ import { NavLink } from 'react-router-dom';
 import axios from 'axios';
 import { apiserver } from '../..';
 import toast from 'react-hot-toast';
+import Loader from '../Loader';
 
 const BODY = styled.body`
   font-family: 'Nunito', serif;
@@ -24,10 +25,12 @@ const LABEL = styled.label`
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [loader, setLoader] = useState(false);
 
   const submitHandler = async e => {
     try {
       e.preventDefault();
+      setLoader(true);
       const { data } = await axios.post(
         `https://backend-thelysian.onrender.com/api/v1/user/login`,
         {
@@ -42,6 +45,7 @@ const Login = () => {
         }
       );
       toast.success(data.message);
+      setLoader(false);
     } catch (error) {
       toast.error(error.response.data.message);
     }
@@ -49,62 +53,68 @@ const Login = () => {
 
   return (
     <BODY>
-      <Container
-        maxH={'container.xl'}
-        rounded={['auto', '3xl']}
-        my={['0', '20']}
-        h={'100vh'}
-        display={'flex'}
-        alignItems={'center'}
-        style={{ 'box-shadow': '10px 10px' }}
-        border="2px solid black"
-        borderRadius={['0', '10px']}
-        bgColor={'whiteAlpha.500'}
-        px={('0', '2rem')}
-        py={'20'}
-        bgGradient="linear(to-l, #20e3b2, #29ffc6)"
-      >
-        <VStack pt={'10'} alignItems={'flex-start'} w={'80'} mx={'auto'}>
-          {' '}
-          <Heading pb={'10'} pt={'10'}>
-            Login
-          </Heading>
-          <form onSubmit={submitHandler}>
-            <LABEL>Email</LABEL>
-            <Input
-              type="email"
-              bgColor={'white'}
-              value={email}
-              onChange={e => setEmail(e.target.value)}
-            />
-            <LABEL>Password</LABEL>
-            <Input
-              type="password"
-              bgColor={'white'}
-              value={password}
-              onChange={e => setPassword(e.target.value)}
-            />
-            <Button
-              w={'80'}
-              alignContent={'center'}
-              style={{ 'box-shadow': '4px 4px' }}
-              border="2px solid black"
-              borderRadius={['0', '10px']}
-              bgColor={'whiteAlpha.500'}
-              bgGradient="linear(to-l, #20e3b2, #29ffc6)"
-              type="submit"
-            >
-              Log In
-            </Button>
-          </form>
-          <Text>
-            New Here{' '}
-            <NavLink style={{ color: '#2563eb' }} to={'/register'}>
-              Register
-            </NavLink>
-          </Text>
-        </VStack>
-      </Container>
+      {loader ? (
+        <Loader />
+      ) : (
+        <>
+          <Container
+            maxH={'container.xl'}
+            rounded={['auto', '3xl']}
+            my={['0', '20']}
+            h={'100vh'}
+            display={'flex'}
+            alignItems={'center'}
+            style={{ 'box-shadow': '10px 10px' }}
+            border="2px solid black"
+            borderRadius={['0', '10px']}
+            bgColor={'whiteAlpha.500'}
+            px={('0', '2rem')}
+            py={'20'}
+            bgGradient="linear(to-l, #20e3b2, #29ffc6)"
+          >
+            <VStack pt={'10'} alignItems={'flex-start'} w={'80'} mx={'auto'}>
+              {' '}
+              <Heading pb={'10'} pt={'10'}>
+                Login
+              </Heading>
+              <form onSubmit={submitHandler}>
+                <LABEL>Email</LABEL>
+                <Input
+                  type="email"
+                  bgColor={'white'}
+                  value={email}
+                  onChange={e => setEmail(e.target.value)}
+                />
+                <LABEL>Password</LABEL>
+                <Input
+                  type="password"
+                  bgColor={'white'}
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
+                />
+                <Button
+                  w={'80'}
+                  alignContent={'center'}
+                  style={{ 'box-shadow': '4px 4px' }}
+                  border="2px solid black"
+                  borderRadius={['0', '10px']}
+                  bgColor={'whiteAlpha.500'}
+                  bgGradient="linear(to-l, #20e3b2, #29ffc6)"
+                  type="submit"
+                >
+                  Log In
+                </Button>
+              </form>
+              <Text>
+                New Here{' '}
+                <NavLink style={{ color: '#2563eb' }} to={'/register'}>
+                  Register
+                </NavLink>
+              </Text>
+            </VStack>
+          </Container>
+        </>
+      )}
     </BODY>
   );
 };
