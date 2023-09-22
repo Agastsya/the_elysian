2e82856dbf9d6ab1a9a23f786ae94a11b8866c42
Agastsya Joshi <agastsya007@gmail.com>
2023-09-22 17:58:15 +0530

    Update Register.jsx

--- src/components/userAccount/Register.jsx
+++ src/components/userAccount/Register.jsx
@@ -50,10 +50,12 @@ const Register = () => {
       );
       toast.success(data.message);
       setLoader(false);
-      setIsAuthenticated(true);
-      navigate('/');
       localStorage.setItem('authToken', data.token);
       toast.success(data.message);
+      setIsAuthenticated(true);
+      navigate('/');
+      window.location.reload();
+      window.location.href = '/';
     } catch (error) {
       toast.error(error.response.data.message);
       setLoader(false);
